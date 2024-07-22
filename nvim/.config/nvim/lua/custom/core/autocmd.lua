11eaca2c6c073a62163533dea44fab417c8957d5
Phúc Nguyễn <40336551+phucnguyen035@users.noreply.github.com>
2024-07-22 17:24:02 +0700

    update wipe windowless buffers command

--- nvim/.config/nvim/lua/custom/core/autocmd.lua
+++ nvim/.config/nvim/lua/custom/core/autocmd.lua
@@ -17,10 +17,10 @@ vim.api.nvim_create_autocmd('TextYankPost', {
 })
 
 vim.api.nvim_create_user_command('WipeWindowlessBufs', function()
-  local bufinfos = vim.fn.getbufinfo { buflisted = true }
+  local bufinfos = vim.fn.getbufinfo { buflisted = 1 }
   vim.tbl_map(function(bufinfo)
     if bufinfo.changed == 0 and (not bufinfo.windows or #bufinfo.windows == 0) then
-      print(('Deleting buffer %d : %s'):format(bufinfo.bufnr, bufinfo.name))
+      vim.notify(('Deleting buffer %d : %s'):format(bufinfo.bufnr, bufinfo.name))
       vim.api.nvim_buf_delete(bufinfo.bufnr, { force = false, unload = false })
     end
   end, bufinfos)
