648a6aee8b6d3277a6af10388158a170c2dfe967
Phúc Nguyễn <40336551+phucnguyen035@users.noreply.github.com>
2024-07-22 15:42:18 +0700

    update telescope find files options

--- nvim/.config/nvim/lua/custom/plugins/telescope.lua
+++ nvim/.config/nvim/lua/custom/plugins/telescope.lua
@@ -17,34 +17,51 @@ return {
       end,
     },
   },
-  opts = {
-    -- [[ Configure Telescope ]]
-    -- See `:help telescope` and `:help telescope.setup()`
-    defaults = {
-      path_display = { 'smart' },
-      layout_strategy = 'flex',
-      mappings = {
-        n = {
-          ['q'] = 'close',
-        },
-        i = {
-          ['<C-q>'] = 'close',
-        },
-      },
-    },
-    extensions = {
-      fzf = {
-        fuzzy = true, -- false will only do exact matching
-        override_generic_sorter = true, -- override the generic sorter
-        override_file_sorter = true, -- override the file sorter
-        case_mode = 'smart_case', -- or "ignore_case" or "respect_case"
-        -- the default case_mode is "smart_case"
-      },
-    },
-  },
-  config = function(_, opts)
+  config = function()
     local telescope = require 'telescope'
-    telescope.setup(opts)
+    local telescopeConfig = require("telescope.config")
+
+    -- Clone the default Telescope configuration
+    local vimgrep_arguments = { unpack(telescopeConfig.values.vimgrep_arguments) }
+
+    -- I want to search in hidden/dot files.
+    table.insert(vimgrep_arguments, "--hidden")
+    -- I don't want to search in the `.git` directory.
+    table.insert(vimgrep_arguments, "--glob")
+    table.insert(vimgrep_arguments, "!**/.git/*")
+
+    telescope.setup({
+      defaults = {
+        -- `hidden = true` is not supported in text grep commands.
+        vimgrep_arguments,
+        path_display = { 'smart' },
+        layout_strategy = 'flex',
+        mappings = {
+          n = {
+            ['q'] = 'close',
+          },
+          i = {
+            ['<C-q>'] = 'close',
+          },
+        },
+      },
+      pickers = {
+        find_files = {
+          -- `hidden = true` will still show the inside of `.git/` as it's not `.gitignore`d.
+          find_command = { "rg", "--files", "--hidden", "--glob", "!**/.git/*" },
+        },
+      },
+      extensions = {
+        fzf = {
+          fuzzy = true,                   -- false will only do exact matching
+          override_generic_sorter = true, -- override the generic sorter
+          override_file_sorter = true,    -- override the file sorter
+          case_mode = 'smart_case',       -- or "ignore_case" or "respect_case"
+          -- the default case_mode is "smart_case"
+        },
+      },
+    })
+
     telescope.load_extension 'fzf'
   end,
   keys = {
