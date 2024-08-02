fa8183322f701671a80c290c960a0b34c3aa72ab
Phúc Nguyễn <40336551+phucnguyen035@users.noreply.github.com>
2024-08-02 13:53:44 +0700

    silent on empty hover

--- nvim/.config/nvim/lua/custom/plugins/ui.lua
+++ nvim/.config/nvim/lua/custom/plugins/ui.lua
@@ -88,6 +88,9 @@ return {
           ['vim.lsp.util.stylize_markdown'] = true,
           ['cmp.entry.get_documentation'] = true, -- requires hrsh7th/nvim-cmp
         },
+        hover = {
+          silent = true,
+        },
       },
       presets = {
         bottom_search = true, -- use a classic bottom cmdline for search
