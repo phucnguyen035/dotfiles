8cc41a693d21fc3575661a60a80ca56239e114f4
Phúc Nguyễn <40336551+phucnguyen035@users.noreply.github.com>
2025-04-14 12:03:02 +0700

    remove nvim lint for go

--- nvim/.config/nvim/lua/custom/plugins/init.lua
+++ nvim/.config/nvim/lua/custom/plugins/init.lua
@@ -7,10 +7,6 @@ return {
     config = function()
       local lint = require 'lint'
 
-      lint.linters_by_ft = {
-        go = { 'golangcilint' },
-      }
-
       vim.api.nvim_create_autocmd({ 'BufWritePost', 'BufEnter' }, {
         group = vim.api.nvim_create_augroup('lint', { clear = true }),
         callback = function()
