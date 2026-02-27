c346e0062405df01f324ff50753f6eee7d263c52
Phúc Nguyễn <40336551+phucnguyen035@users.noreply.github.com>
2026-02-27 14:50:51 +0700

    update lsp

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -4,8 +4,18 @@ return {
     opts = {
       setup = {
         vtsls = function(_, opts)
+          opts.settings.javascript = {
+            tsserver = { maxTsServerMemory = 16184 },
+          }
+
+          opts.settings.typescript.tsserver = { maxTsServerMemory = 16184 }
           opts.settings.typescript.inlayHints.parameterTypes.enabled = false
           opts.settings.typescript.inlayHints.functionLikeReturnTypes.enabled = false
+        end,
+        eslint = function(_, opts)
+          opts.settings.codeActionOnSave = {
+            enable = true,
+          }
         end,
       },
       servers = {
