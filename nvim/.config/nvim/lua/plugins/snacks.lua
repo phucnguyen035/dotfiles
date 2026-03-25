c18adac6ea777c5b7ec834c2ba366139feb9823f
Phúc Nguyễn <40336551+phucnguyen035@users.noreply.github.com>
2026-03-25 14:22:24 +0700

    remove notifier opt

--- nvim/.config/nvim/lua/plugins/snacks.lua
+++ nvim/.config/nvim/lua/plugins/snacks.lua
@@ -2,9 +2,6 @@ return {
   "folke/snacks.nvim",
   ---@type snacks.Config
   opts = {
-    notifier = {
-      level = vim.log.levels.ERROR,
-    },
     scroll = {
       animate = {
         fps = 120,
