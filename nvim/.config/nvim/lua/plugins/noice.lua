963a1a334684053b8f42010faf2719a66ecb5eb0
Phúc Nguyễn <40336551+phucnguyen035@users.noreply.github.com>
2026-03-31 13:59:07 +0700

    filter out no information available msg

--- nvim/.config/nvim/lua/plugins/noice.lua
+++ nvim/.config/nvim/lua/plugins/noice.lua
@@ -6,5 +6,16 @@ return {
     presets = {
       lsp_doc_border = true,
     },
+    routes = {
+      {
+        filter = {
+          event = "notify",
+          find = "No information available",
+        },
+        opts = {
+          skip = true,
+        },
+      },
+    },
   },
 }
