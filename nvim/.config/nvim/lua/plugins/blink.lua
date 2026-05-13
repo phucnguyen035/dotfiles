d51b08264e1142b41f87c5acd03b722cffbfece0
Phúc Nguyễn <40336551+phucnguyen035@users.noreply.github.com>
2026-05-13 13:13:02 +0700

    add blink fuzzy sorts

---
+++ nvim/.config/nvim/lua/plugins/blink.lua
@@ -0,0 +1,15 @@
+return {
+  "saghen/blink.cmp",
+  ---@module 'blink.cmp'
+  ---@type blink.cmp.Config
+  opts = {
+    fuzzy = {
+      sorts = {
+        "exact",
+        -- defaults
+        "score",
+        "sort_text",
+      },
+    },
+  },
+}
