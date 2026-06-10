df9ed39e800f18c3bf57cd8f8c455fc576e12814
Phúc Nguyễn <40336551+phucnguyen035@users.noreply.github.com>
2026-06-10 15:36:21 +0700

    remove vtsls and eslint lsp config
    oxlint/biome and tsgo tbd

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -1,23 +1,9 @@
 return {
   {
     "neovim/nvim-lspconfig",
+    ---@module 'nvim-lspconfig'
     opts = {
-      setup = {
-        vtsls = function(_, opts)
-          opts.settings.javascript = {
-            tsserver = { maxTsServerMemory = 16184 },
-          }
-
-          opts.settings.typescript.tsserver = { maxTsServerMemory = 16184 }
-          opts.settings.typescript.inlayHints.parameterTypes.enabled = false
-          opts.settings.typescript.inlayHints.functionLikeReturnTypes.enabled = false
-        end,
-        eslint = function(_, opts)
-          opts.settings.codeActionOnSave = {
-            enable = true,
-          }
-        end,
-      },
+      setup = {},
       servers = {
         ["*"] = {
           keys = {
