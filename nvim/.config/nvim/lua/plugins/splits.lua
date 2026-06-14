35d2b03d6eafc6ea8899139e688d3d9684839118
Phúc Nguyễn <40336551+phucnguyen035@users.noreply.github.com>
2026-06-14 17:26:04 +0700

    update smart splits

--- nvim/.config/nvim/lua/plugins/splits.lua
+++ nvim/.config/nvim/lua/plugins/splits.lua
@@ -1,8 +1,66 @@
+---@module "lazy"
+---@type LazySpec[]
 return {
   {
     "mrjones2014/smart-splits.nvim",
     opts = {
-      zellij_move_focus_or_tab = true,
+      ignored_filetypes = { "snacks_picker_list" },
+    },
+    keys = {
+      {
+        "<C-h>",
+        function()
+          require("smart-splits").move_cursor_left()
+        end,
+      },
+      {
+        "<C-l>",
+        function()
+          require("smart-splits").move_cursor_right()
+        end,
+      },
+      {
+        "<C-j>",
+        function()
+          require("smart-splits").move_cursor_down()
+        end,
+      },
+      {
+        "<C-k>",
+        function()
+          require("smart-splits").move_cursor_up()
+        end,
+      },
+      {
+        "<C-\\>",
+        function()
+          require("smart-splits").move_cursor_previous()
+        end,
+      },
+      {
+        "<A-h>",
+        function()
+          require("smart-splits").resize_left()
+        end,
+      },
+      {
+        "<A-l>",
+        function()
+          require("smart-splits").resize_right()
+        end,
+      },
+      {
+        "<A-j>",
+        function()
+          require("smart-splits").resize_down()
+        end,
+      },
+      {
+        "<A-k>",
+        function()
+          require("smart-splits").resize_up()
+        end,
+      },
     },
   },
 }
