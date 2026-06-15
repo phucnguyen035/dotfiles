82dd0c4c8968a306ef60e2a8ea36dc859c5a64ff
Phúc Nguyễn <40336551+phucnguyen035@users.noreply.github.com>
2026-06-15 20:12:29 +0700

    refactor conform

--- nvim/.config/nvim/lua/plugins/conform.lua
+++ nvim/.config/nvim/lua/plugins/conform.lua
@@ -1,14 +1,41 @@
+---@param opts conform.setupOpts
+local setup_ts_formatter = function(opts)
+  local support = {
+    "javascript",
+    "javascriptreact",
+    "typescript",
+    "typescriptreact",
+  }
+
+  opts.formatters_by_ft = opts.formatters_by_ft or {}
+
+  for _, ft in ipairs(support) do
+    opts.formatters_by_ft[ft] = opts.formatters_by_ft[ft] or {}
+    ---@diagnostic disable-next-line: param-type-mismatch
+    table.insert(opts.formatters_by_ft[ft], "biome-check")
+  end
+
+  opts.formatters = opts.formatters or {}
+  opts.formatters["biome-check"] = {
+    require_cwd = true,
+  }
+end
+
+---@param opts conform.setupOpts
+local setup_python_formatter = function(opts)
+  opts.formatters_by_ft = opts.formatters_by_ft or {}
+
+  opts.formatters_by_ft.python = {
+    "ruff_fix",
+    "ruff_format",
+    "ruff_organize_imports",
+  }
+end
+
 return {
   "stevearc/conform.nvim",
-  ---@type conform.setupOpts
-  opts = {
-    formatters_by_ft = {
-      python = {
-        "ruff_fix",
-        "ruff_format",
-        "ruff_organize_imports",
-      },
-    },
-    formatters = {},
-  },
+  opts = function(_, opts)
+    setup_ts_formatter(opts)
+    setup_python_formatter(opts)
+  end,
 }
