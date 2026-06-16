814806dd34f83aed1cf79ea35931cfae1e933968
Phúc Nguyễn <40336551+phucnguyen035@users.noreply.github.com>
2026-06-16 10:47:44 +0700

    feat(nvim): config tsgo type hints

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -5,6 +5,33 @@ return {
     opts = {
       setup = {},
       servers = {
+        tsgo = {
+          settings = {
+            typescript = {
+              inlayHints = {
+                enumMemberValues = {
+                  enabled = false,
+                },
+                functionLikeReturnTypes = {
+                  enabled = false,
+                },
+                parameterNames = {
+                  enabled = "literals",
+                  suppressWhenArgumentMatchesName = true,
+                },
+                parameterTypes = {
+                  enabled = false,
+                },
+                propertyDeclarationTypes = {
+                  enabled = true,
+                },
+                variableTypes = {
+                  enabled = false,
+                },
+              },
+            },
+          },
+        },
         ["*"] = {
           keys = {
             { "<C-k>", false, mode = "i" },
